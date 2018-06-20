e5247cae41430524985f701237074850906352eb
Gabriel-Calin Lazar <gabriel.calin.lazar@gmail.com>
2018-06-20 18:32:35 +0300

    Create links and extract URL params

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react';
-import './App.css';
+import { BrowserRouter as Router, match as Match, Route, Switch } from 'react-router-dom';
 
-import logo from './logo.svg';
+// import './App.css';
+// import logo from './logo.svg';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React App</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Users = () => <h1>Users</h1>;
+const User = ({ match }: { match: Match<{userId:string}> }) => (
+  <>
+    <h1>User</h1>
+    <div>
+      <h3>Id: {match.params.userId}</h3>
+    </div>
+  </>
+);
+
+const App = () => (
+  <Router>
+    <Switch>
+      {/* TODO lazy load */}
+      <Route exact path="/" component={Users} />
+      <Route path="/user/:userId(\d+)" component={User} />
+    </Switch>
+  </Router>
+);
 
 export default App;
